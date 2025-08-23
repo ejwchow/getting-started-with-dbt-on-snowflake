8f6c18b029f2d213a438f5ed87684daa88c8620a
Edmond Chow <23467927+ejwchow@users.noreply.github.com>
2025-08-22 18:30:25 -0700

    Update dbt_sandbox.sql
    added cleanup code

--- tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -1,3 +1,9 @@
+-- Check tables exist
 SHOW TABLES IN DATABASE tasty_bytes_dbt_db;
 
+-- Check views exist
 SHOW VIEWS IN DATABASE tasty_bytes_dbt_db;
+
+-- Clean up databases
+DROP DATABASE IF EXISTS tasty_bytes_dbt_db;
+DROP DATABASE IF EXISTS tb_101;
